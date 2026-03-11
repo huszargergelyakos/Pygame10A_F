678cb6844b00f163849487240a018dbf0f1cfb95
huszargergelyakos <huszar.gergely.akos@students.jedlik.eu>
2026-03-11 09:23:05 +0100

    Starting the coding

---
+++ settings.py
@@ -0,0 +1,14 @@
+# Képernyő beállítások (PC-re lehet, hogy érdemes lesz nagyobbra venni, pl. 400x700)
+SCREEN_WIDTH: int = 288
+SCREEN_HEIGHT: int = 512
+FPS: int = 60
+
+# Színek (RGB)
+BLUE: tuple[int, int, int] = (53, 81, 92)
+RED: tuple[int, int, int] = (255, 0, 0)
+YELLOW: tuple[int, int, int] = (255, 255, 0)
+WHITE: tuple[int, int, int] = (255, 255, 255)
+
+# Játék specifikus beállítások
+LANE_POSITIONS: list[int] = [50, 95, 142, 190]
+PLAYER_SPEED: int = 5
