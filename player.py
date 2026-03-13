01d3417acc3c2088c215c569daf7da4852be4513
huszargergelyakos <huszar.gergely.akos@students.jedlik.eu>
2026-03-13 13:13:29 +0100

    Player class

---
+++ player.py
@@ -0,0 +1,32 @@
+import pygame
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x: int, y: int, image_path: str) -> None:
+        super().__init__()
+        # Kép betöltése és beállítása
+        raw_image = pygame.image.load(image_path).convert_alpha()
+        self.image = pygame.transform.scale(raw_image, (90, 100))  # Eredeti méretezés
+
+        # Pozíció és hitbox (rect)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def update(self, move_left: bool, move_right: bool) -> None:
+        # A játékos mozgatása a bemenetek alapján
+        if move_left:
+            self.rect.x -= 5
+        if move_right:
+            self.rect.x += 5
+        self._check_boundaries()
+
+    def _check_boundaries(self) -> None:
+        # Ne menjen le a képernyőről (a 288 a képernyő szélessége)
+        if self.rect.left < 30:
+            self.rect.left = 30
+        if self.rect.right > 288 - 30:
+            self.rect.right = 288 - 30
+
+    def draw(self, screen: pygame.Surface) -> None:
+        # Játékos kirajzolása
+        screen.blit(self.image, self.rect)
