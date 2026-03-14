651fc3962e2d6bf19bc5bac9ec533854952bd6be
huszargergelyakos <huszar.gergely.akos@students.jedlik.eu>
2026-03-14 21:11:26 +0100

    new backgrounds

--- road.py
+++ road.py
@@ -1,22 +1,25 @@
 import pygame
-from settings import SCREEN_WIDTH, SCREEN_HEIGHT
+from settings import SCREEN_HEIGHT, ROAD_WIDTH, ROAD_X
 
 
 class Road:
     def __init__(self, image_path: str) -> None:
-        # Kép betöltése és átméretezése
         raw_image = pygame.image.load(image_path).convert_alpha()
-        self.image = pygame.transform.scale(raw_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.image = pygame.transform.scale(raw_image, (ROAD_WIDTH, SCREEN_HEIGHT))
+        self.y1: int = 0
+        self.y2: int = -SCREEN_HEIGHT
 
         # Kezdőpozíciók inicializálása (így már nem jelez hibát a linter)
         self.y1: int = 0
         self.y2: int = -SCREEN_HEIGHT
 
     def update(self, speed: int) -> None:
-        # Az út mozgatása lefelé
         self.y1 += speed
         self.y2 += speed
-        self._check_boundaries()
+        if self.y1 >= SCREEN_HEIGHT:
+            self.y1 = self.y2 - SCREEN_HEIGHT
+        if self.y2 >= SCREEN_HEIGHT:
+            self.y2 = self.y1 - SCREEN_HEIGHT
 
     def _check_boundaries(self) -> None:
         # Pontos illeszkedés: a másik útdarab tetejéhez fűzzük hozzá
@@ -26,6 +29,6 @@ class Road:
             self.y2 = self.y1 - SCREEN_HEIGHT
 
     def draw(self, screen: pygame.Surface) -> None:
-        # Az út kirajzolása
-        screen.blit(self.image, (0, self.y1))
-        screen.blit(self.image, (0, self.y2))
+        # Most már a ROAD_X (középső) pozícióba rajzolunk
+        screen.blit(self.image, (ROAD_X, self.y1))
+        screen.blit(self.image, (ROAD_X, self.y2))
