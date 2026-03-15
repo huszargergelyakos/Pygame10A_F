ef3d0d50c5fe2dcd98ccf66f0428c975c5fb06ed
huszargergelyakos <huszar.gergely.akos@students.jedlik.eu>
2026-03-15 12:34:37 +0100

    General code and background massive upgrades

--- road.py
+++ road.py
@@ -4,31 +4,25 @@ from settings import SCREEN_HEIGHT, ROAD_WIDTH, ROAD_X
 
 class Road:
     def __init__(self, image_path: str) -> None:
+        # Út képének betöltése és méretezése
         raw_image = pygame.image.load(image_path).convert_alpha()
         self.image = pygame.transform.scale(raw_image, (ROAD_WIDTH, SCREEN_HEIGHT))
-        self.y1: int = 0
-        self.y2: int = -SCREEN_HEIGHT
-
-        # Kezdőpozíciók inicializálása (így már nem jelez hibát a linter)
-        self.y1: int = 0
-        self.y2: int = -SCREEN_HEIGHT
+        self.y1, self.y2 = 0, -SCREEN_HEIGHT
 
     def update(self, speed: int) -> None:
+        # Az út darabok mozgatása lefelé
         self.y1 += speed
         self.y2 += speed
-        if self.y1 >= SCREEN_HEIGHT:
-            self.y1 = self.y2 - SCREEN_HEIGHT
-        if self.y2 >= SCREEN_HEIGHT:
-            self.y2 = self.y1 - SCREEN_HEIGHT
+        self._wrap()
 
-    def _check_boundaries(self) -> None:
-        # Pontos illeszkedés: a másik útdarab tetejéhez fűzzük hozzá
+    def _wrap(self) -> None:
+        # Pozíciók alaphelyzetbe állítása a végtelen görgetéshez
         if self.y1 >= SCREEN_HEIGHT:
             self.y1 = self.y2 - SCREEN_HEIGHT
         if self.y2 >= SCREEN_HEIGHT:
             self.y2 = self.y1 - SCREEN_HEIGHT
 
     def draw(self, screen: pygame.Surface) -> None:
-        # Most már a ROAD_X (középső) pozícióba rajzolunk
+        # Mindkét útszelet kirajzolása
         screen.blit(self.image, (ROAD_X, self.y1))
         screen.blit(self.image, (ROAD_X, self.y2))
--- settings.py
+++ settings.py
@@ -1,15 +1,17 @@
-SCREEN_WIDTH: int = 900  # Megnövelt szélesség a háttér miatt
-SCREEN_HEIGHT: int = 960
-FPS: int = 60
+# Képernyő méretek és képfrissítés
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 960
+FPS = 60
 
 # Színek
-BLUE: tuple[int, int, int] = (53, 81, 92)
+WHITE = (255, 255, 255)
 
-# Út beállítások - Középre igazítva
-ROAD_WIDTH: int = 540
-ROAD_X: int = (SCREEN_WIDTH - ROAD_WIDTH) // 2
+# Út beállításai (középre igazítva)
+ROAD_WIDTH = 540
+ROAD_X = (SCREEN_WIDTH - ROAD_WIDTH) // 2
 
-# 4 sáv koordinátái az ÚTON BELÜL (hozzáadva az út kezdőpontját)
-LANE_POSITIONS: list[int] = [ROAD_X + 100, ROAD_X + 212, ROAD_X + 325, ROAD_X + 435]
+# Sávok pontos koordinátái az úton
+LANE_POSITIONS = [ROAD_X + 100, ROAD_X + 212, ROAD_X + 325, ROAD_X + 435]
 
-BIOME_DISTANCE: int = 500 # Ennyi megtett egység után váltunk hátteret
+# Játékmenet konstansok
+DISTANCE_PER_BIOME = 1000
